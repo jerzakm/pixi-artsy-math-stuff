47733ac387e7a61c964259f657c3ec662daa81f1
Marcin <jerzakm@gmail.com>
2019-08-24 21:50:03 +0200

    fourier working

--- src/06_fourierDrawing/FourierDrawing.ts
+++ src/06_fourierDrawing/FourierDrawing.ts
@@ -15,30 +15,38 @@ export class FourierDrawing extends Container {
   }
 
   public animate(delta: number) {
-    const r = 100
     this.g.clear()
-    this.g.lineStyle(2, 0xFFFFFF)
-    this.g.drawCircle(0, 0, r)
+    this.angl += 0.001
 
     const point = {
-      x: r * Math.cos(this.angl),
-      y: r * Math.sin(this.angl)
+      x: 0,
+      y: 0
     }
 
-    this.wave.unshift(point.y)
+    const f = 10
+
+    this.g.lineStyle(1, 0xFFFFFF)
+
+    for (let i = 0; i < f; i++) {
+      let prevx = point.x
+      let prevy = point.y
+      let n = i * 2 + 1
+      const r = 160 * (4 / (n * Math.PI))
+      this.g.drawCircle(point.x, point.y, r)
+
+      point.x += r * Math.cos(n * this.angl)
+      point.y += r * Math.sin(n * this.angl)
+
+      i + 1 == f ? this.wave.unshift(point.y) : false
+    }
+
     this.wave.length > 800 ? this.wave.pop() : false
 
-    this.g.moveTo(0, 0)
-    this.g.lineTo(point.x, point.y)
-
-    this.g.lineStyle(0)
-
-    this.angl += 0.03
     this.g.beginFill(0xFFAA12)
     this.g.drawCircle(point.x, point.y, 5)
     this.g.endFill()
 
-    const lineShift = 2 * r
+    const lineShift = 2 * 160
 
     this.g.moveTo(point.x, point.y)
     this.g.lineStyle(1, 0xAAAAAA)
