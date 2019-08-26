7c2e7a4f2b35d27e682f9ba26a2a1b9ba763b27c
Marcin <jerzakm@gmail.com>
2019-08-27 01:27:26 +0200

    test line render

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "../src/_scss/main.scss"
 import * as renderer from './core/renderer'
 import * as Stats from 'stats.js'
-import { Container, Graphics, GraphicsGeometry, RENDERER_TYPE } from "pixi.js"
+import { Container, Graphics, GraphicsGeometry, RENDERER_TYPE, Loader, Sprite, interaction } from "pixi.js"
 import { polarCoords, Point } from './math/coordMath'
 import { ILissajousTableOptions, LissajousTable } from "./01_lissajous/LissajousTable"
 import { CirclePacking } from "./02_circlePacking/CirclePacking"
@@ -11,6 +11,8 @@ import { RadarFragment } from "./05_radarFragment/RadarFragment";
 import { FourierSeries } from "./06_fourierDrawing/FourierSeries";
 import { FourierDrawing } from "./06_fourierDrawing/FourierDrawing";
 
+export const loader = Loader.shared
+
 renderer.initRenderer()
 
 var stats = new Stats.default()
@@ -18,70 +20,127 @@ stats.showPanel(0)
 document.body.appendChild(stats.dom)
 
 
-// LISSAJOUS SETUP
-// const helperCanvas = document.createElement('canvas')
-// document.body.appendChild(helperCanvas)
-// helperCanvas.width = renderer.renderer.width
-// helperCanvas.height = renderer.renderer.height
-// helperCanvas.style.position = 'fixed'
-// helperCanvas.style.top = '0'
-// helperCanvas.style.left = '0'
-// const options: ILissajousTableOptions = {
-//     gridSize: 25,
-//     cellSize: 48,
-//     pointCircleR: 5,
-//     rotationSpeed: 0.0015,
-//     helperCanvas: helperCanvas
-// }
 
-// const lissajousTable = new LissajousTable(options)
-// lissajousTable.x = 50
-// lissajousTable.y = 50
-// renderer.stage.addChild(lissajousTable)
+loader.add('test', 'test.jpg').load(() => {
+    const graphics = new Graphics()
+    graphics.position.x = 50
+    const sprite = Sprite.from(loader.resources['test'].texture)
+    sprite.x = 50
+    renderer.stage.addChild(sprite)
+    renderer.stage.addChild(graphics)
+    const pixels = renderer.renderer.extract.pixels(sprite)
+    sprite.alpha = 0
 
-// CIRCLE PACKING SETUP
-// const circlePacking = new CirclePacking()
-// circlePacking.x = 0
-// circlePacking.y = 0
-// renderer.stage.addChild(circlePacking)
+    const avgPixelColor = (pixels.reduce((prev, current) => current += prev)) / pixels.length
+    const lineCount = 20
+
+    const getColorIndicesForCoord = (x: number, y: number, width: number) => {
+        const red = y * (width * 4) + x * 4;
+        return [red, red + 1, red + 2, red + 3];
+    };
+
+    let divider = avgPixelColor
+
+    renderer.ticker.add((delta) => {
+        stats.begin()
+        graphics.clear()
+        // divider < 0 ? divider = 500 : divider -= 3
+        // simpleWaves()
+        // weightedWaves()
+        bezierWeightedWaves()
 
 
-// GIFT WRAPPING SETUP
-// const giftWrapping = new GiftWrapping(300, 300, 15)
-// giftWrapping.x = 100
-// giftWrapping.y = 100
-// renderer.stage.addChild(giftWrapping)
+        stats.end()
+    })
 
-// const windmill = new Windmill(7)
-// renderer.stage.addChild(windmill)
-// windmill.x = 100
-// windmill.y = 50
 
-// const radar = new RadarFragment(window.innerHeight/2-50, 5)
-// radar.x = 100
-// radar.y = 50
-// renderer.stage.addChild(radar)
+    const bezierWeightedWaves = () => {
+        const verticalStep = 30
+        const horizontalStep = 20
+        for (let y = 0; y < sprite.height; y += verticalStep) {
+            graphics.moveTo(0, y)
+            for (let x = 0; x < sprite.width; x += horizontalStep) {
+                let colorHeight = 0
 
-// const fourierSeries = new FourierSeries()
-// fourierSeries.x = 300
-// fourierSeries.y = 300
-// renderer.stage.addChild(fourierSeries)
+                const heightPoints: HeightHelper[] = []
 
-const fourierDrawing = new FourierDrawing()
-fourierDrawing.x = 50
-fourierDrawing.y = 100
-renderer.stage.addChild(fourierDrawing)
+                for (let vert = x; vert < x + horizontalStep; vert++) {
+                    for (let h = y; h < y + verticalStep; h++) {
+                        const indices = getColorIndicesForCoord(x, h, sprite.width)
+                        const r = pixels[indices[0]]
+                        const g = pixels[indices[1]]
+                        const b = pixels[indices[2]]
+                        const a = pixels[indices[3]]
+                        colorHeight += (r + g + b)
+                    }
+                    colorHeight = colorHeight / verticalStep / 3 / divider
+                    heightPoints.push({
+                        x: vert,
+                        h: colorHeight
+                    })
+                    colorHeight == 1 ? graphics.lineStyle(1, 0xFFFFFF, 1) : graphics.lineStyle(1, 0xFFFFFF, 1)
+                }
+                heightPoints.sort((a, b) => a.h - b.h)
+                const smallest = heightPoints[0]
+                const largest = heightPoints[heightPoints.length - 1]
+                const pts = [smallest, largest]
 
-renderer.ticker.add((delta) => {
-    stats.begin()
+                pts.sort((a, b) => a.x - b.x)
 
-    // lissajousTable.animate()
-    // circlePacking.animate(delta)
-    // giftWrapping.animate(delta)
-    // windmill.animate(delta)
-    // radar.animate(delta)
-    // fourierSeries.animate(delta)
-    fourierDrawing.animate(delta)
+                graphics.bezierCurveTo(
+                    pts[1].x, pts[1].h * horizontalStep + y,
+                    pts[0].x, pts[0].h * horizontalStep + y,
+                    heightPoints[heightPoints.length - 1].x, heightPoints[heightPoints.length - 1].h * horizontalStep + y,
+                )
 
-    stats.end()
+            }
+        }
+        graphics.lineStyle(0)
+    }
+
+    const weightedWaves = () => {
+        const step = 30
+        for (let y = 0; y < sprite.height; y += step) {
+            graphics.moveTo(0, y)
+            for (let x = 0; x < sprite.width; x += 2) {
+                let colorHeight = 0
+                for (let h = y; h < y + step; h++) {
+                    const indices = getColorIndicesForCoord(x, h, sprite.width)
+                    const r = pixels[indices[0]]
+                    const g = pixels[indices[1]]
+                    const b = pixels[indices[2]]
+                    const a = pixels[indices[3]]
+                    colorHeight += (r + g + b)
+                }
+                colorHeight = colorHeight / step / 3 / divider
+                colorHeight == 1 ? graphics.lineStyle(1, 0xFFFFFF, 1) : graphics.lineStyle(1, 0xFFFFFF, 1)
+                graphics.lineTo(x, y + colorHeight * step)
+            }
+        }
+        graphics.lineStyle(0)
+    }
+
+    const simpleWaves = () => {
+        const step = 30
+        for (let y = 0; y < sprite.height; y += step) {
+            graphics.moveTo(0, y)
+            for (let x = 0; x < sprite.width; x += 5) {
+                const indices = getColorIndicesForCoord(x, y, sprite.width)
+                const r = pixels[indices[0]]
+                const g = pixels[indices[1]]
+                const b = pixels[indices[2]]
+                const a = pixels[indices[3]]
+                const colorHeight = (r + g + b) / 3 / 255
+                colorHeight == 1 ? graphics.lineStyle(1, 0xFFFFFF, 1) : graphics.lineStyle(1, 0xFFFFFF, 1)
+                graphics.lineTo(x, y + colorHeight * step)
+            }
+        }
+        graphics.lineStyle(0)
+    }
 })
+
+
+interface HeightHelper {
+    x: number,
+    h: number
+}
